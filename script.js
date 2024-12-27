bdae48d9d78b122ded5c41525af19316977f30a4
Deep092006 <deepsurati94@gmail.com>
2024-12-27 19:57:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let calc = {
+  add(a,b){
+    console.log(`${a + b}`);
+  },
+  sub(a,b){
+    console.log(`${a - b}`);
+  },
+  mult(a,b){
+    console.log(`${a * b}`);
+  },
+  div(a,b){
+    console.log(`${a / b}`);
+  }
+}
+calc.mult(5,3);
